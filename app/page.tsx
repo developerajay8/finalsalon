96ce8c5a89829151fb6e4ce11090fd6e38291f66
Ajaysharma <akpancholi8@gmail.com>
2026-04-21 12:49:58 +0530

    menucommitfinal

--- app/page.tsx
+++ app/page.tsx
@@ -2,13 +2,14 @@ import Image from "next/image";
 import BarberLandingPage from "./components/barberlandingpage";
 import AboutServices from "./components/aboutsection";
 import GallerySection from "./components/gallerysection";
-
+import MenuSection from "./components/menusections";
 export default function Home() {
   return (
     <div>
       <BarberLandingPage/>
       <AboutServices/>
       <GallerySection/>
+      <MenuSection/>
     </div>
   );
 }
